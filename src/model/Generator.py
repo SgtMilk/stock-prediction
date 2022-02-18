fixed bad prediction scaling and added different prediction length techniques

src/model/Generator.py
@@ -35,7 +35,8 @@ class Generator(Module):
         for _ in range(self.output_dim):
             prediction = self.forward_helper(input_data[:,-self.input_dim:])
             input_data = torch.cat((input_data, torch.reshape(prediction, (prediction.shape[0], 1, 1))), axis=1)  
-        return input_data[:,-self.output_dim:]
+        returned_data = input_data[:,-self.output_dim:]
+        return returned_data
 
     def forward_helper(self, input_data):
         """

src/model/Net.py
@@ -94,21 +94,24 @@ class Net:
                     x_train, y_train = self.dataset.get_train(batch)
 
                     labels_size_d = y_train.size(0)
-                    real_labels = torch.full((labels_size_d,), 1, dtype=torch.float, device=self.device)
-                    fake_labels = torch.full((labels_size_d,), 0, dtype=torch.float, device=self.device)
+                    real_labels = torch.full((labels_size_d,), 0, dtype=torch.float, device=self.device)
+                    fake_labels = torch.full((labels_size_d,), 1, dtype=torch.float, device=self.device)
 
                     ####################################
                     # Update the discriminator network #
                     ####################################
 
-                    self.discriminator.zero_grad()
-                    self.generator.zero_grad()
+                    self.optimizer_g.zero_grad()
+                    self.optimizer_d.zero_grad()
 
                     # Train with all-real batch
                     output_real_d = self.discriminator(torch.cat((x_train, y_train), 1)).view(-1)
                     error_real_d = self.criterion_d(output_real_d, real_labels)
                     error_real_d.backward()
                     d_x += output_real_d.mean().item()
+
+                    self.optimizer_d.step()
+                    self.optimizer_d.zero_grad()
 
                     # Train with all-fake (noise) batch
                     fake = self.generator(x_train)
@@ -182,14 +185,16 @@ class Net:
             for batch in range(self.dataset.num_test_batches): 
                 if batch == 0:
                     x_test, y_test, y_unscaled_test = self.dataset.get_test(batch)
-                    predicted_y_test = torch.squeeze(self.generator(x_test))
+                    predicted_y_test = self.generator(x_test)
+                    predicted_y_test = torch.reshape(predicted_y_test, (predicted_y_test.shape[0], predicted_y_test.shape[1]))
 
                     y_test = y_test.detach().cpu().numpy()
                     y_unscaled_test = y_unscaled_test.detach().cpu().numpy()
                     predicted_y_test = predicted_y_test.detach().cpu().numpy()
                 else:
                     x_temp, y_temp, y_unscaled_temp = self.dataset.get_test(batch)
-                    predicted_y_temp = torch.squeeze(self.generator(x_temp))
+                    predicted_y_temp = self.generator(x_temp)
+                    predicted_y_temp = torch.reshape(predicted_y_temp, (predicted_y_temp.shape[0], predicted_y_temp.shape[1]))
                     y_test = np.concatenate((y_test, y_temp.detach().cpu().numpy()))
                     y_unscaled_test = np.concatenate((y_unscaled_test, y_unscaled_temp.detach().cpu().numpy()))
                     predicted_y_test = np.concatenate((predicted_y_test, predicted_y_temp.detach().cpu().numpy()))
@@ -206,6 +211,10 @@ class Net:
         """
         This method will save the trained model according to the dataset's code(s) and the current date
         """
+        if os.path.exists(self.generator_filepath):
+            os.remove(self.generator_filepath)
+        if os.path.exists(self.discriminator_filepath):
+            os.remove(self.discriminator_filepath)
         torch.save(self.generator.state_dict(), self.generator_filepath)
         torch.save(self.discriminator.state_dict(), self.discriminator_filepath)
 

src/predict.py
@@ -24,7 +24,7 @@ def predict_stock(code, interval: int, num_days: int, overwrite: bool = False):
         destination_folder, f"generator-{str(interval)}.hdf5")
 
     # getting the data
-    dataset = Dataset(code, interval=interval, y_flag=True)
+    dataset = Dataset(code, interval=interval, y_flag=True, pred_length=GAN.pred_length, look_back=GAN.look_back)
     dataset.transform_to_torch()
 
     # getting our model and net
@@ -38,19 +38,30 @@ def predict_stock(code, interval: int, num_days: int, overwrite: bool = False):
     generator.load_state_dict(torch.load(filepath))
 
     data = torch.from_numpy(np.array(dataset.x.detach().cpu().numpy())).float().to(device=GAN.device)
-    data = torch.unsqueeze(data[-1], dim=2)
+    data = torch.reshape(data[-1], (1, dataset.look_back, 1))
     
     generator.eval()
+    returned_data = None
+
     with torch.set_grad_enabled(False):
-        prediction = generator(data).squeeze()
+        if dataset.pred_length == 1:
+            for _ in range(num_days):
+                prediction = generator(data[:,-dataset.look_back:])
+                prediction = torch.reshape(prediction, (prediction.shape[0], prediction.shape[1]))
+                data = torch.cat((data, torch.reshape(prediction, (prediction.shape[0], 1, 1))), axis=1) 
+                returned_data = data[:,-num_days:] 
+        else:
+            returned_data = generator(data).squeeze()
 
     # re-transforming to numpy
-    predicted = prediction.detach().cpu().numpy()[:num_days + 1].squeeze()
+    predicted = returned_data.detach().cpu().numpy().squeeze()
 
-    return inverse_scaling(predicted, dataset)
+    returned_data = inverse_scaling(predicted, dataset)[:num_days + 1]
+
+    return returned_data
 
 def inverse_scaling(scaled_data, dataset):
     """inverses the scaling from the dataset"""
-    first_unscaling = dataset.inverse_transform(scaled_data)
-    scaling_factor = dataset.y_unscaled.detach().cpu().numpy()[-1, 0] - first_unscaling[0]
+    first_unscaling = dataset.inverse_transform(scaled_data.squeeze())
+    scaling_factor = dataset.y_unscaled.detach().cpu().numpy()[-1, 0] - first_unscaling.squeeze()[0]
     return first_unscaling + scaling_factor